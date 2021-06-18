cc7d00fd59ce5b5c23707dc27ee6a73925cc8b3c
Linda Zou <49109592+lindafz@users.noreply.github.com>
2021-06-17 18:08:51 -0700

    Comment out spring boot context for LoginApplicationTest

--- src/test/java/com/lindazf/login/jwt/LoginApplicationTest.java
+++ src/test/java/com/lindazf/login/jwt/LoginApplicationTest.java
@@ -7,9 +7,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 //@ExtendWith(SpringExtension.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+// @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class LoginApplicationTest {
-	
+
 	@Test
 	void contextLoads() {
 	}
