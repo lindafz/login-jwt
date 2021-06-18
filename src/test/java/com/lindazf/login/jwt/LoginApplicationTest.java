7b731a0b37e05a4ba6ecfb730614ccbd663db505
Linda Zou <49109592+lindafz@users.noreply.github.com>
2021-06-17 18:03:57 -0700

    Add document

--- src/test/java/com/lindazf/login/jwt/LoginApplicationTest.java
+++ src/test/java/com/lindazf/login/jwt/LoginApplicationTest.java
@@ -7,7 +7,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 //@ExtendWith(SpringExtension.class)
-//@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class LoginApplicationTest {
 	
 	@Test
